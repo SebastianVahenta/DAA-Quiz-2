ced223184b7213bf8bc26ac6839221d11dbe87bc
dababayou <bayunismaranagatama036@gmail.com>
2025-05-29 22:14:35 +0700

    utils carve

---
+++ utils/get_neighbours.py
@@ -0,0 +1,20 @@
+def get_neighbours(maze, i, j, visited):
+# i: the current row
+# j: the current column
+# visited: cells that have been visited
+
+  neighbours = []
+
+  if i > 1 and [i-2, j] not in visited:
+    neighbours.append([i-2, j])
+  
+  if i < len(maze)-2 and [i+2, j] not in visited:
+    neighbours.append([i+2, j])
+
+  if j > 1 and [i, j-2] not in visited:
+    neighbours.append([i, j-2])
+
+  if j < len(maze[0])-2 and [i, j+2] not in visited:
+    neighbours.append([i, j+2])
+
+  return neighbours
