f73305d0dfc031331bf61b407a9a0587c61a3019
dababayou <bayunismaranagatama036@gmail.com>
2025-05-29 22:03:10 +0700

    input main - bayu

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+
+from utils.generate_grid import generate_grid
+from utils.print_maze import print_maze
+from utils.carve import carve
+from utils.modify import modify
+from algorithms.dijkstra_maze_solver import dijkstra_maze_solver
+
+def main():
+
+  m = 21
+  n = 21
+  maze = []
+
+  maze = generate_grid(n, m, maze)
+  carve(maze)
+  modify(maze, m, n)
+  
+  print("The maze:")
+  print_maze(maze)
+
+  maze_for_dijkstra = maze.copy()
+
+  print("Solved maze with Dijkstra:")
+  start_time_dijkstra = time.time()
+  dijkstra_maze_solver(maze_for_dijkstra, [1, 1], [m-2, n-2])
+  print("Execution time Dijkstra: %s seconds" % (time.time() - start_time_dijkstra))
+  print_maze(maze_for_dijkstra)
+
+if __name__ == "__main__":
+  main()
